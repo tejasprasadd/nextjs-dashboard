301fe451d721484d8e31b53f200a8027cf372b6c
Tejas Prasad <tejasprasad43@gmail.com>
2025-03-23 23:43:12 +0530

    First Commit

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
+import { lusitana } from "@/app/ui/fonts";
+import "@/app/ui/global.css";
 import Search from '@/app/ui/search';
 import {
   CustomersTableType,
